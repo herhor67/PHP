3a24891393d6723ca55fa54b150021811ebf8e96
herhor67 <herhor67@interia.pl>
2019-04-17 21:41:49 +0200

    Create array_orderby.php

---
+++ array_orderby.php
@@ -0,0 +1,23 @@
+<?php
+
+//sort two-dimensional array by columns
+//eg. array_orderby($arr, 'name', SORT_ASC, 'age', SORT_DESC, 'skill', SORT_DESC)
+
+function array_orderby()
+	{
+		$args = func_get_args();
+		$data = array_shift($args);
+		foreach ($args as $n => $field) {
+			if (is_string($field)) {
+				$tmp = array();
+				foreach ($data as $key => $row)
+					$tmp[$key] = $row[$field];
+				$args[$n] = $tmp;
+				}
+		}
+		$args[] = &$data;
+		call_user_func_array('array_multisort', $args);
+		return array_pop($args);
+	}
+
+?>
