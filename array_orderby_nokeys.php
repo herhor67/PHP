7b1630f8ce37141131ae7a926133d94c0b0ed0b3
herhor67 <herhor67@interia.pl>
2019-04-17 21:53:21 +0200

    Create array_orderby_nokeys.php

---
+++ array_orderby_nokeys.php
@@ -0,0 +1,21 @@
+<?php
+
+//same as array_orderby_keys, but this one rearranges keys
+function array_orderby_nokeys()
+{
+	$args = func_get_args();
+	$data = array_shift($args);
+	foreach ($args as $n => $field) {
+		if (is_string($field)) {
+			$tmp = array();
+			foreach ($data as $key => $row)
+				$tmp[$key] = $row[$field];
+			$args[$n] = $tmp;
+		}
+	}
+	$args[] = &$data;
+	call_user_func_array('array_multisort', $args);
+	return array_values(array_pop($args));
+}
+
+?>
