cf8577fcf00f48df48a11f34832e5337c5e93e1a
herhor67 <herhor67@interia.pl>
2020-02-03 11:34:54 +0100

    Create str_contains_all.php

---
+++ string/str_contains_all.php
@@ -0,0 +1,12 @@
+<?php
+
+function str_contains_all(string $haystack, array $needles)
+{
+	foreach ($needles as $needle)
+		if (strpos($haystack, $needle) === false)
+			return false;
+	}
+	return true;
+}
+
+?>
