6da474ec03af525f79c4e3708f0c6b6fd33fa482
herhor67 <herhor67@interia.pl>
2020-02-13 19:33:28 +0100

    Update imagettftext_blur_sf.php

--- graphics/imagettftext_blur_sf.php
+++ graphics/imagettftext_blur_sf.php
@@ -1,5 +1,15 @@
 <?php
 
+define('ALIGN_LEFT',   0);
+define('ALIGN_CENTER', 1);
+define('ALIGN_RIGHT',  2);
+
+function textSize(float $size, float $angle, string $font, string $text)
+{
+	$f = imagettfbbox($size, $angle, $font, $text);
+	return ['height' => $f[1] - $f[5], 'width' => $f[4] - $f[0]];
+}
+
 function imagettftextblur(&$img, float $size, float $angle, int $x, int $y, int $shdwclr, string $fontfile, string $text, int $align=ALIGN_LEFT, int $shdwrds=0)
 {
 	if ($align)
