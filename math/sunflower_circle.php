0ce3b82ac1bb2b0e6384cf31e9ecd5f179c66805
herhor67 <herhor67@interia.pl>
2020-02-13 19:15:38 +0100

    Update sunflower_circle.php
    Generates N evenly distributed points inside unit circle.
    Original code @ https://stackoverflow.com/a/28572551/7321403

--- math/sunflower_circle.php
+++ math/sunflower_circle.php
@@ -1,4 +1,6 @@
 <?php
+//Generates N evenly distributed points inside unit circle.
+//Original code @ https://stackoverflow.com/a/28572551/7321403
 
 function sunflower($n, $alpha)
 {
@@ -7,19 +9,11 @@ function sunflower($n, $alpha)
 	$phi = (sqrt(5)+1)/2;
 	for ($k=1; $k<=$n; ++$k)
 	{
-		$r = radius($k, $n, $b);
+		$r = ($k > $n-$b) ? 1 : (sqrt($k-1/2)/sqrt($n-($b+1)/2));
 		$theta = 2*M_PI*$k/$phi**2;
 		$points[] = [$r*cos($theta), $r*sin($theta)];
 	}
 	return $points;
-}
-
-function radius($k, $n, $b)
-{
-	if ($k > $n-$b)
-		return 1;
-	else
-		return sqrt($k-1/2)/sqrt($n-($b+1)/2);
 }
 
 ?>
